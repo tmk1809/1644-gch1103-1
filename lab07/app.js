88614bc0d0edfc77e8d79d5657fd6d1ff6759e85
= <=>
2023-08-03 08:49:59 +0700

    lab7

--- lab07/app.js
+++ lab07/app.js
@@ -9,23 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-//1. khai báo mongoose 
-var mongoose = require('mongoose');
-//note: cần ghi rõ tên của DB ở cuối url của connection string. ví dụ: "demo"
-var db = "mongodb+srv://longndt:wxpY5GLB4We8hTDQ@cluster0.cc35aqx.mongodb.net/demo";
-
-mongoose.connect(db)
-.then(() => console.log ('connect to db ok !'))
-.catch((err) => console.log ('connect to db error !'));
-
-//2. khai báo body-parser
+//1. cấu hình body-parser (lấy input data từ form)
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended : false}));
 
-//3. khai báo dateFormat của HBS
-var hbs = require('hbs');
-hbs.registerHelper('dateFormat', require('handlebars-dateformat')); 
-
+//2. cấu hình mongoose (làm việc và tương tác với DB)
+var mongoose = require('mongoose');
+//note: cần khai báo db name trong uri. vd: "gch1103"
+var db = "mongodb+srv://longndt:wxpY5GLB4We8hTDQ@cluster0.cc35aqx.mongodb.net/gch1103";
+mongoose.connect(db)
+.then(() => console.log('ok'))
+.catch((error) => console.log('failed'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,7 +50,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//cần đổi port của server khi deploy web lên cloud
-app.listen(process.env.PORT || 3001);
+//3. cấu hình port cho web server để deploy lên Render
+var port = process.env.PORT || 3001;
+app.listen(port);
 
 module.exports = app;
